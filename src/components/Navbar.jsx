753bffb8bf3320b51cd961c36dff3c325f2756d4
u9urturk <u9urturk@gmail.com>
2024-03-26 11:24:21 +0300

    Reafactoring

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,21 +31,21 @@ export default function Navbar() {
       console.log(scrollY)
     return (
         <div className={classNames({
-            "navbar bg-transparent transition-all hover:opacity-100 cursor-pointer  mt-4 fixed z-40":true,
+            "navbar bg-transparent transition-all flex items-center justify-between hover:opacity-100 cursor-pointer  mt-4 fixed z-40":true,
             "opacity-0":scrollY!=0,
             
         })}>
-            <div className="flex-1">
+            <div className="flex items-center justify-center">
                 <a className="btn btn-ghost normal-case font-serif text-xl">Uğur TÜRK</a>
             </div>
-            <div className='flex-auto font-serif gap-x-12'>
+            <div className='flex items-center justify-center font-serif gap-x-12'>
               <button onClick={() => scrollToSection("about")}  className='hover:scale-90 transition-all  btn btn-outline' href="">Hakkımda</button>
                 <button  className='hover:scale-90 transition-all  btn btn-outline' href="">Beceriler</button>
                 <button onClick={() => scrollToSection("portfolyo")} className='hover:scale-90 transition-all  btn btn-outline' href="">Portfolyo</button>             
                 <button  className='hover:scale-90 transition-all  btn btn-outline' href="">İletişim</button>
             </div>
             <div className="flex-none gap-2">
-                <div className="form-control">
+                <div className="form-control hidden">
                     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
                 </div>
                 <label className="swap swap-rotate">
@@ -60,7 +60,7 @@ export default function Navbar() {
                     <svg className="swap-off fill-current w-8 h-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" /></svg>
 
                 </label>
-                <div className="dropdown  dropdown-end">
+                <div className="dropdown hidden  dropdown-end">
 
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-16 rounded-full">
